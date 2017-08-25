53849391e5a47baf4d6f3bc8bc4aaad1ea43004f
pricew826 <wprice989@gmail.com>
2017-08-25 09:34:11 -0400

    8/25 Added Firebase

--- Faces of the Holocaust Photographic History/AppDelegate.swift
+++ Faces of the Holocaust Photographic History/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+
         
         //This changes the background color of the Tab bar to Black
         UITabBar.appearance().barTintColor = .black
